2ae0b2a1c2077c31ecbaad20fa56db4eb247c0b7
jamelswift <Wipatsasicha0702@gmail.com>
2025-02-08 20:58:25 +0700

    66030281 วิภัสศศิชา

---
+++ Lab09_ex02/Program.cs
@@ -0,0 +1,12 @@
+﻿Person.name = "wipatsasicha";  
+Console.WriteLine(Person.GetName());  
+
+static class Person
+{
+    public static string? name;  
+    
+    public static string GetName()  
+    {
+        return $"Hello from {name}";  
+    }
+}
---
+++ Lab09_ex03/Program.cs
@@ -0,0 +1,19 @@
+﻿Person.name = "Wipatsasicha"; 
+System.Console.WriteLine(Person.GetName());  
+var student = new Student();
+Student.name = "Wipatsasicha";
+Console.WriteLine(Student.GetName());
+
+ class Person
+{
+    public static string? name;  
+    public static string GetName()
+    {
+        return $"Hello from {name}";
+    }
+}
+
+class Student : Person
+{
+    
+}
